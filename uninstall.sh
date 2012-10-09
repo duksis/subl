0ff42f5b532496e7b8d6b02aa718489e3a294109
Hugo Duksis <hugo.duksis@tieto.com>
2012-10-09 22:32:03 +0300

    Remove files marked for removal if removal file exists

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,9 @@
 
 sudo rm -fr "/Applications/Sublime Text 2.app"
 
-source $HOME/.sublime/uninstall_config.sh
+if [ -f "$HOME/.sublime/uninstall_config.sh" ]; then
+  source $HOME/.sublime/uninstall_config.sh
+fi
 rm -fr $HOME/.sublime
 
 source $HOME/.bash_profile
