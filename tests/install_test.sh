2488dc72dfcbde33e45086d60b453dd0a776d22a
Hugo Duksis <hugo.duksis@tieto.com>
2012-10-17 08:58:58 +0300

    changing tests to run in verbose mode

--- tests/install_test.sh
+++ tests/install_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. $(dirname $0)/support/assert.sh
+. $(dirname $0)/support/assert.sh -v
 
 assert "echo" "test"
 assert_end $(basename $0 _test.sh)
--- tests/uninstall_test.sh
+++ tests/uninstall_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. $(dirname $0)/support/assert.sh
+. $(dirname $0)/support/assert.sh -v
 
 assert "echo"
 assert_end $(basename $0 _test.sh)
